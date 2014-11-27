4156a263e43d6417a437d27fd5a6cb2d1e34a5b6
Mikael Couzic <mikaelcouzic@gmail.com>
2014-11-27 18:10:38 +0100

    Prod conf

--- config/env/production.js
+++ config/env/production.js
@@ -1,6 +1,7 @@
 module.exports = {
     port: process.env.OPENSHIFT_NODEJS_PORT || 8080,
     address : process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1",
+    TOKEN_SECRET: 'ADD_YOUR_SECRET_HERE',
     db : {
         address : 'mongodb://localhost/code-exam',
         user : '',
